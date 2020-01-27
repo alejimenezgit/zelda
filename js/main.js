601a9e839ac677bffc62a6134a944fa6b86399fa
alejandro <alejimenezgitgithub@gmail.com>
2020-01-27 23:44:41 +0100

    base table game

--- js/main.js
+++ js/main.js
@@ -7,25 +7,47 @@ document.addEventListener('DOMContentLoaded', (event) => {
     let splash = document.getElementById('splash');
 
     // splash screen
-    window.onload = function() { document.onkeypress = mostrarInformacionCaracter; }
+    window.onload = function() { document.onkeypress = knowKey; }
+    printGame();
 
     // añadirlo en game?
-    function mostrarInformacionCaracter(evObject) {
-        var elCaracter = String.fromCharCode(evObject.which); 
-        if(elCaracter === 'e'){
-            game.classList.remove('hidden');
-            splash.classList.add('hidden');
+    function knowKey(evObject) {
+        switch(String.fromCharCode(evObject.which))
+        {
+            case 'e':
+                skipScreen();
+                break;
+            default:
+                break;
         }
     }
 
-    // pintar pantalla del juego
-    function printGameOver(){
+    function skipScreen(){
+        game.classList.remove('hidden');
+        splash.classList.add('hidden');
+    }
 
+    // pintar pantalla del juego
+    function printGame (){
+        var tbdy = document.createElement('tbody');
+        tbdy.id = "idtbody";
+        var total = 0;
+        for (let x = 0; x < 10; x++) {
+          var tr = document.createElement('tr');
+          for (var y = 1; y <= 17; y++) {
+              var td = document.createElement('td');
+              td.id = total;
+              total++;
+              tr.appendChild(td);
+          }
+          tbdy.appendChild(tr);
+        }
+        game.append(tbdy);
     }
 
     // empezar
     function start(){
-
+        
     }
 
     // pasar valores necesarios a game.js
